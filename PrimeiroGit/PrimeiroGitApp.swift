7dc87cbf3e305f75c2f2f7b75233cd8a38d1315a
Matias Cazarotto <matias.cazarotto@hotmail.com>
2023-02-22 20:55:03 -0300

    Initial Commit

---
+++ PrimeiroGit/PrimeiroGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrimeiroGitApp.swift
+//  PrimeiroGit
+//
+//  Created by Matias Cazarotto on 22/02/23.
+//
+
+import SwiftUI
+
+@main
+struct PrimeiroGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
